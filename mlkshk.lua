e71ebca97b990870d7ec7543fa3e122367082322
Arkiver2 <Arkiver@hotmail.com>
2017-04-04 22:14:56 +0200

    initial

---
+++ mlkshk.lua
@@ -0,0 +1,234 @@
+dofile("urlcode.lua")
+dofile("table_show.lua")
+
+local item_type = os.getenv('item_type')
+local item_value = os.getenv('item_value')
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+
+local allowed_strings = {}
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+
+local disco_users = {}
+
+allowed_strings[item_value] = true
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url, parenturl)
+  if string.match(url, "'+")
+     or string.match(url, "[<>\\]")
+     or string.match(url, "//$")
+     or string.match(url, "icons%.fallback%.css")
+     or string.match(url, "^https?://[^/]*%.s%.mlkshk%.com")
+     or string.match(url, "sign%-in")
+     or not string.match(url, "^https?://[^/]*mlkshk%.com") then
+    return false
+  end
+
+  if item_type == "posts" then
+    for s in string.gmatch(url, "([0-9A-Z]+)[0-9A-Z]") do
+      if allowed_strings[s] == true then
+        return true
+      end
+    end
+  elseif item_type == "user" then
+    for s in string.gmatch(url, "([a-zA-Z0-9%-_]+)") do
+      if allowed_strings[s] == true then
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  if item_type == "posts" and string.match(url, "profile%.jpg$") then
+    return false
+  end
+
+  if (downloaded[url] ~= true and addedtolist[url] ~= true)
+     and (allowed(url, parent["url"]) or html == 0) then
+    addedtolist[url] = true
+    return true
+  end
+
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+
+  downloaded[url] = true
+  
+  local function check(urla)
+    local origurl = url
+    local url = string.match(urla, "^([^#]+)")
+    if (downloaded[url] ~= true and addedtolist[url] ~= true)
+       and allowed(url, origurl) then
+      table.insert(urls, { url=string.gsub(url, "&amp;", "&") })
+      addedtolist[url] = true
+      addedtolist[string.gsub(url, "&amp;", "&")] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/\\/") then
+      check(string.match(url, "^(https?:)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(string.match(url, "^(https?:)")..newurl)
+    elseif string.match(newurl, "^\\/") then
+      check(string.match(url, "^(https?://[^/]+)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^/") then
+      check(string.match(url, "^(https?://[^/]+)")..newurl)
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(string.match(url, "^(https?://[^%?]+)")..newurl)
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?")
+       or string.match(newurl, "^[/\\]")
+       or string.match(newurl, "^[jJ]ava[sS]cript:")
+       or string.match(newurl, "^[mM]ail[tT]o:")
+       or string.match(newurl, "^vine:")
+       or string.match(newurl, "^android%-app:")
+       or string.match(newurl, "^%${")) then
+      check(string.match(url, "^(https?://.+/)")..newurl)
+    end
+  end
+  
+  if allowed(url, nil) then
+    html = read_file(file)
+
+    if string.match(url, "^https?://[^/]*mlkshk%.com/p/[0-9A-Z]+$")
+       and status_code == 200 then
+      local post_id = string.match(url, "^https?://[^/]*mlkshk.com/p/([0-9A-Z]+)$")
+      check("https://s.mlkshk.com/r/" .. post_id)
+      check("https://mlkshk.com/p/" .. post_id .. "/likes")
+      check("https://mlkshk.com/p/" .. post_id .. "/saves")
+      check("https://mlkshk.com/p/" .. post_id .. "/quick-comments")
+    end
+
+    for user in string.gmatch(html, '"user_name"%s*:%s*"([^"]+)"') do
+      disco_users[user] = true
+    end
+    for user in string.gmatch(html, "/user/([a-zA-Z0-9%-_]+)") do
+      disco_users[user] = true
+    end
+
+    for newurl in string.gmatch(html, '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ":%s*url%(([^%)]+)%)") do
+      check(newurl)
+    end
+  end
+
+  return urls
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if (status_code >= 200 and status_code <= 399) then
+    downloaded[url["url"]] = true
+    downloaded[string.gsub(url["url"], "https?://", "http://")] = true
+  end
+
+  if abortgrab == true then
+    io.stdout:write("ABORTING...\n")
+    return wget.actions.ABORT
+  end
+  
+  if status_code >= 500 or
+    (status_code >= 400 and status_code ~= 404 and status_code ~= 410) or
+    status_code == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    os.execute("sleep 1")
+    tries = tries + 1
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      if allowed(url["url"], nil) then
+        return wget.actions.ABORT
+      else
+        return wget.actions.EXIT
+      end
+    else
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab == true then
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local file = io.open(item_dir..'/'..warc_file_base..'_data.txt', 'w')
+  for user, _ in pairs(disco_users) do
+    file:write("user:" .. user .. "\n")
+  end
+  file:close()
+end
